3a01d4e9d5170c8369227c6b36d873aee9764b3b
Ramzi <hadad@MacBook-Pro-van-Ramzi.local>
2026-02-28 13:04:46 +0100

    Further reduce opacity of vintage hero photo to 5% for extreme subtlety

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -22,7 +22,7 @@ export const HeroSection = () => {
     >
       {/* 1. Historical Black & White Photo (Left on Desktop, Top on Mobile) */}
       <div
-        className="absolute inset-0 w-full md:w-[70%] h-full z-0 pointer-events-none opacity-[0.15] [mask-image:linear-gradient(to_bottom,black_10%,transparent_80%)] md:[mask-image:linear-gradient(to_right,black_10%,transparent_80%)] [-webkit-mask-image:linear-gradient(to_bottom,black_10%,transparent_80%)] md:[-webkit-mask-image:linear-gradient(to_right,black_10%,transparent_80%)]"
+        className="absolute inset-0 w-full md:w-[70%] h-full z-0 pointer-events-none opacity-[0.05] [mask-image:linear-gradient(to_bottom,black_10%,transparent_80%)] md:[mask-image:linear-gradient(to_right,black_10%,transparent_80%)] [-webkit-mask-image:linear-gradient(to_bottom,black_10%,transparent_80%)] md:[-webkit-mask-image:linear-gradient(to_right,black_10%,transparent_80%)]"
         style={{
           backgroundImage: 'url("/images/vintage-mushroom.png")',
           backgroundSize: 'cover',
