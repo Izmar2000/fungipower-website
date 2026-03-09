6603c0cf400f1b979726e88274008f159ac11acc
Ramzi <hadad@MBP-van-Ramzi.home>
2026-03-09 13:07:55 +0100

    force clean layout test

--- src/app/[lang]/theroadto/page.tsx
+++ src/app/[lang]/theroadto/page.tsx
@@ -1,9 +1,7 @@
 export default function Page() {
   return (
     <div style={{ padding: "100px", fontSize: "40px" }}>
-      Theroadto werkt weer
+      THEROADTO PAGE WORKS
     </div>
   )
 }
-
-
