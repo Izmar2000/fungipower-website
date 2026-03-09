61d8ae804baaf750a09ad8e2f5341a177dfd0dbb
Ramzi <hadad@MBP-van-Ramzi.home>
2026-03-09 21:27:28 +0100

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,3 @@
-nano next.config.tsconst nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/theroadto",
-        destination: "/theroadto/index.html",
-        permanent: false,
-      },
-    ];
-  },
-};
-
-export default nextConfig;
 const nextConfig = {
   async redirects() {
     return [
