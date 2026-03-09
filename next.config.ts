deddfecf6743c3993c712c60bf3f8fc2ec216431
Ramzi <hadad@MBP-van-Ramzi.home>
2026-03-09 14:33:33 +0100

    add theroadto rewrite

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,12 @@
-const nextConfig: any = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+const nextConfig = {
   async rewrites() {
     return [
-      /*
       {
-        source: '/:lang/theroadto',
-        destination: '/theroadto/index.html',
+        source: "/theroadto",
+        destination: "/theroadto/index.html",
       },
-      {
-        source: '/theroadto',
-        destination: '/theroadto/index.html',
-      },
-      {
-        source: '/:lang/theroadto/_next/:path*',
-        destination: '/theroadto/_next/:path*',
-      },
-      {
-        source: '/theroadto/_next/:path*',
-        destination: '/theroadto/_next/:path*',
-      }
-      */
-    ]
+    ];
   },
 };
+
 export default nextConfig;
